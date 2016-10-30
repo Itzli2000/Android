6d859ae8f31125b9c78872c9aff6543a915539da
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-10-30 15:02:48 -0600

    Hola mundo
    Primer App, conocer sistema Android Studio

---
+++ HolaMundo/app/src/main/java/com/itzli/holamundo/model/Alumno.java
@@ -0,0 +1,38 @@
+package com.itzli.holamundo.model;
+
+import java.util.Date;
+
+/**
+ * Created by itzli on 19/10/2016.
+ */
+
+public class Alumno {
+
+    private String nombre;
+    private long numero_cuenta;
+    private Date fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getNumero_cuenta() {
+        return numero_cuenta;
+    }
+
+    public void setNumero_cuenta(long numero_cuenta) {
+        this.numero_cuenta = numero_cuenta;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
