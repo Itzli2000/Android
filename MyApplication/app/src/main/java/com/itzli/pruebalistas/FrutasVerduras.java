486c0542322d34389a6c0439b385d7073bdae67f
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-10-30 17:49:12 -0600

    Primer programa de curso android platzi
    Revisar dependencias

---
+++ MyApplication/app/src/main/java/com/itzli/pruebalistas/FrutasVerduras.java
@@ -0,0 +1,20 @@
+package com.itzli.pruebalistas;
+
+/**
+ * Created by itzli on 21/10/2016.
+ */
+
+public class FrutasVerduras {
+
+    public int icon;
+    public String title;
+    public FrutasVerduras(){
+        super();
+    }
+
+    public FrutasVerduras(int icon, String title){
+        super();
+        this.icon = icon;  //Lo que recibimos...
+        this.title = title;//nos lo asignamos
+    }
+}
