ec24dc4c581003f5adb35ab4fee1f06e105b147e
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-11-03 14:14:10 -0600

    Agregado comportamiento de cronometro, modificando vista del mapa

--- DabbaApp/app/src/main/java/com/tesca/dabbaapp/HomeFragment.java
+++ DabbaApp/app/src/main/java/com/tesca/dabbaapp/HomeFragment.java
@@ -1,13 +1,24 @@
 package com.tesca.dabbaapp;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -18,12 +29,64 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private SupportMapFragment mSupportMapFragment;
 
+
+    Fragment fragment =  null;
+    TextView textView;
+    View root;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        root = inflater.inflate(R.layout.fragment_home,container, false);
+
+        //Controles para mapa
+        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getChildFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.mapFragment, mSupportMapFragment).commit();
+        }null
+
+        if (mSupportMapFragment != null) {
+            mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    if (googleMap != null) {
+                        googleMap.getUiSettings().setAllGesturesEnabled(true);
+                        if (ActivityCompat.checkSelfPermission(getContext(),
+                                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        googleMap.setMyLocationEnabled(true);
+                        LatLng sydney = new LatLng(-34, 151);
+                        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    }
+                }
+            });
+        }
+
+        //Controles para cronometro
+        textView = (TextView)root.findViewById(R.id.chronometer2);
+
+        new CountDownTimer(30000, 1000){
+            public void onTick (long millisUntilFinished){
+                textView.setText("" + millisUntilFinished / 1000);
+            }
+            public void onFinish(){
+                textView.setText("Fin del tiempo");
+            }
+        }.start();
+
+
+
+
+        return root;
+
     }
 
 }
