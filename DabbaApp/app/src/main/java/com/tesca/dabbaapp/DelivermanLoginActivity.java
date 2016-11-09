545dc919310d9b83fc6a49e76a3054e02600f0ca
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-11-09 11:26:32 -0600

    Trabajando en identificacion con base de datos Firebase

--- DabbaApp/app/src/main/java/com/tesca/dabbaapp/DelivermanLoginActivity.java
+++ DabbaApp/app/src/main/java/com/tesca/dabbaapp/DelivermanLoginActivity.java
@@ -1,11 +1,23 @@
 package com.tesca.dabbaapp;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,8 +26,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.tesca.dabbaapp.Deliverman_control_views.DeliverNavigationActivity;
 
+import java.security.PrivateKey;
+
 public class DelivermanLoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "SignInActivity";
+    private String mCustomToken;
+    private Button mSignInButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     FirebaseDatabase mDatabase;
 
     @Override
@@ -23,34 +42,114 @@ public class DelivermanLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deliverman_login);
 
+        //Asignar boton "Ingresa" a variable
+        mSignInButton = (Button) findViewById(R.id.btn_login);
+
         //Inicializar base de datos
         mDatabase = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-        //Escribir en de base datos
-        DatabaseReference a =  mDatabase.getReference().child("Delivery").child("Test1");
-        mDatabase.getReference().child("Delivery").child("Test1").setValue("abcdef");
 
-        //Leer desde base de datos
-        DatabaseReference b = mDatabase.getReference().push();//Genera una clave unica
-        b.setValue("Test");//Escribe informacion en la clave generada
-
-        a.addValueEventListener(new ValueEventListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String test = dataSnapshot.getValue(String.class);
-
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
             }
+        };
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+        mAuth.signInWithCustomToken(mCustomToken)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCustomToken:onComplete:" + task.isSuccessful());
 
-            }
-        });
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCustomToken", task.getException());
+                            Toast.makeText(DelivermanLoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
 
         showToolbar(getResources().getString(R.string.toolbar_deliver_login),true);
 
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        // ...
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+        // ...
+    }
+
+    private void startSignIn() {
+        // Initiate sign in with custom token
+        // [START sign_in_custom]
+        mAuth.signInWithCustomToken(mCustomToken)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCustomToken:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCustomToken", task.getException());
+                            Toast.makeText(DelivermanLoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        // [END sign_in_custom]
+    }
+
+
+    private void setCustomToken(String token) {
+        mCustomToken = token;
+
+        String status;
+        if (mCustomToken != null) {
+            status = "Token:" + mCustomToken;
+        } else {
+            status = "Token: null";
+        }
+
+        // Enable/disable sign-in button and show the token
+        findViewById(R.id.btn_login).setEnabled((mCustomToken != null));
+    }
+
+
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btn_login) {
+            startSignIn();
+
+        }
+    }
+
 
     public void showToolbar (String tittle, boolean upButton){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
