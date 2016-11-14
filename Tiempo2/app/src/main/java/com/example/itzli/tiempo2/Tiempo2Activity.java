cb706f7f89728b6dad5f81e36bb070acd1142d07
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-11-14 13:30:33 -0600

    Pruebas con Timepicker

--- Tiempo2/app/src/main/java/com/example/itzli/tiempo2/Tiempo2Activity.java
+++ Tiempo2/app/src/main/java/com/example/itzli/tiempo2/Tiempo2Activity.java
@@ -1,13 +1,38 @@
 package com.example.itzli.tiempo2;
 
+import android.app.TimePickerDialog;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class Tiempo2Activity extends AppCompatActivity {
 
+    private TimePicker timePicker;
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiempo2);
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
     }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
+    }
+
 }
