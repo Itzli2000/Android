e99b7c60ccc24182cfbf87d4cea6559c90366283
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-11-14 15:20:41 -0600

    Iniciando es sistema de alarma

--- Tiempo2/app/src/main/java/com/example/itzli/tiempo2/Tiempo2Activity.java
+++ Tiempo2/app/src/main/java/com/example/itzli/tiempo2/Tiempo2Activity.java
@@ -6,16 +6,17 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class Tiempo2Activity extends AppCompatActivity {
+public class Tiempo2Activity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
 
     private TimePicker timePicker;
-
-
+    private Button btn;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -23,16 +24,21 @@ public class Tiempo2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiempo2);
 
-        timePicker = (TimePicker) findViewById(R.id.timePicker);
-        // Use the current time as the default values for the picker
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
+        int hour = 0;
+        int min = 0;
 
+        //Inicializar TimePicker con la hora actual
+        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentApiVersion > android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            hour = timePicker.getHour();
+            min = timePicker.getMinute();
+        }
     }
 
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        // Do something with the time chosen by the user
-    }
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+        btn = (Button) findViewById(R.id.alarmBtn);
 
+
+    }
 }
