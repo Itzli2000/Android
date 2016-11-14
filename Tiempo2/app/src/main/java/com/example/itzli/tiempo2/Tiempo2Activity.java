a129509eea7158448d302c93194ac0ac4507f502
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-11-14 12:21:22 -0600

    Iniciando pruebas con Timepicker

---
+++ Tiempo2/app/src/main/java/com/example/itzli/tiempo2/Tiempo2Activity.java
@@ -0,0 +1,13 @@
+package com.example.itzli.tiempo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tiempo2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiempo2);
+    }
+}
