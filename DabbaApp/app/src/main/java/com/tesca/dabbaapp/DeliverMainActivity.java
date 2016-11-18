1513e86c598bad20766b7eca56a4a4dc2a2247f5
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2016-11-17 19:07:14 -0600

    Added Java classes to manifest

--- DabbaApp/app/src/main/java/com/tesca/dabbaapp/DeliverMainActivity.java
+++ DabbaApp/app/src/main/java/com/tesca/dabbaapp/DeliverMainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.tesca.dabbaapp.Deliverman_control_views.DelivermanEmailActivity;
 
 public class DeliverMainActivity extends AppCompatActivity {
 
--- DabbaApp/app/src/main/java/com/tesca/dabbaapp/LoginActivity.java
+++ DabbaApp/app/src/main/java/com/tesca/dabbaapp/LoginActivity.java
@@ -2,12 +2,9 @@ package com.tesca.dabbaapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 import com.tesca.dabbaapp.Deliverman_control_views.DelivermanEmailActivity;
 import com.tesca.dabbaapp.Deliverman_control_views.DelivermanGoogleActivity;
@@ -26,13 +23,13 @@ public class LoginActivity extends AppCompatActivity {
 
     public void goEmailLog(View view) {
 
-        Intent intent = new Intent(this, DelivermanEmailActivity.class);
+        Intent intent = new Intent(LoginActivity.this, DelivermanEmailActivity.class);
         startActivity(intent);
     }
 
     public void GoGoogleLog(View view) {
 
-        Intent intent = new Intent(this, DelivermanGoogleActivity.class);
+        Intent intent = new Intent(LoginActivity.this, DelivermanGoogleActivity.class);
         startActivity(intent);
     }
 }
